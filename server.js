de2374e75ab0bbe47ed601c7e29ef9c895ccfebd
Rémi Rossello <70855637+Remi-Rossello@users.noreply.github.com>
2025-11-17 15:16:04 +0000

    new file:   node_modules/.package-lock.json

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
 
 // Start server
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
