5e998cc553f9f96338279952a216819a705dc5fa
Remi <remi.rossello@gmail.com>
2025-12-11 13:51:35 +0100

    First local commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = 80;
 
 app.use(express.static('FRONTEND'));
 
+// Define test routes
 app.get('/hello', (req, res) => {
   res.send('Hello World! YES IT WORKS');
 });
@@ -12,6 +13,13 @@ app.get('/hello_test', (req, res) => {
   res.send('Hello test');
 });
 
+// Favicon route
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(__dirname + '/FRONTEND/favicon.ico');
+});
+
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
