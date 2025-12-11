02e7927f7d4112a3883ab116c3b83953c7ad4f26
Remi <remi.rossello@gmail.com>
2025-12-11 15:19:50 +0100

    new file:   FRONTEND/background.jpg 	modified:   FRONTEND/index.html 	modified:   FRONTEND/style.css 	modified:   server.js

--- server.js
+++ server.js
@@ -4,23 +4,9 @@ const port = 80;
 
 app.use(express.static('FRONTEND'));
 
-// Define test routes
-app.get('/hello', (req, res) => {
-  res.send('Hello World! YES IT WORKS');
-});
-
-app.get('/hello_test', (req, res) => {
-  res.send('Hello test');
-});
-
 // Favicon route
 app.get('/favicon.ico', (req, res) => {
   res.sendFile(__dirname + '/FRONTEND/favicon.ico');
-});
-
-// Computer route
-app.get('/computers', (req, res) => {
-  res.send('Not available yet');
 });
 
 // Start the server
